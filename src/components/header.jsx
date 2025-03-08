54aba2e3d673dfb29a7438310fec50126c7e4760
Nitesh-DK <nittesh.dk@gmail.com>
2025-03-08 10:33:24 +0530

    clereek is intergerated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import {
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import React from "react";
 
 const header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="flex justify-end items-center p-4 gap-4 h-16">
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+        <UserButton />
+      </SignedOut>
+    </header>
+  );
+};
 
-export default header
+export default header;
