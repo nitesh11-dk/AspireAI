d3d9c42c49db9cb4136ab995f4380f81d80a7775
Nitesh-DK <nittesh.dk@gmail.com>
2025-03-08 10:59:10 +0530

    done but siggnes In is not working

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div  className=''>
-       <Button variant="outline">Outline</Button>
-    </div>
-  )
+    <div className="">{/* <Button variant="outline">Outline</Button> */}</div>
+  );
 }
-
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,34 @@
 import {
+  SignedIn,
   SignedOut,
   SignInButton,
   SignUpButton,
-  UserButton,
+  UserButton
 } from "@clerk/nextjs";
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const header = () => {
   return (
-    <header className="flex justify-end items-center p-4 gap-4 h-16">
-      <SignedOut>
+    <header className="flex justify-between items-center p-4 gap-4 h-16">
+      <nav>
+        <Link href={'/'}>
+        <Image src={'/logo.png'} width={200} height={200} alt="
+        seensai logo" className="h-12 py-1 w-auto object-contain"/>
+        </Link>
+
+        <SignedIn>
+          <h2>hello</h2>
+        </SignedIn>
+      </nav>
+
+     <div className="flex gap-4 items-center">
+     <SignedOut>
         <SignInButton />
         <SignUpButton />
         <UserButton />
       </SignedOut>
+     </div>
     </header>
   );
 };
