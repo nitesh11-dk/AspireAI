b9ff69260531fd1d9bb279ba7ef05c847452fde4
Nitesh-DK <nittesh.dk@gmail.com>
2025-03-08 10:04:41 +0530

    done basic  projects

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div  className=''>
+       <Button variant="outline">Outline</Button>
+    </div>
+  )
+}
+
