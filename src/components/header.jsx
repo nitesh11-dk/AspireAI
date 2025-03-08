8ec60dba8872a68ee99c38b62e7858dccc9545bb
Nitesh-DK <nittesh.dk@gmail.com>
2025-03-08 20:16:54 +0530

    done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
+'use client'
 import {
   SignedIn,
   SignedOut,
   SignInButton,
   SignUpButton,
-  UserButton
+  UserButton,
 } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,9 +13,15 @@ const header = () => {
   return (
     <header className="flex justify-between items-center p-4 gap-4 h-16">
       <nav>
-        <Link href={'/'}>
-        <Image src={'/logo.png'} width={200} height={200} alt="
-        seensai logo" className="h-12 py-1 w-auto object-contain"/>
+        <Link href={"/"}>
+          <Image
+            src={"/logo.png"}
+            width={200}
+            height={200}
+            alt="
+        seensai logo"
+            className="h-12 py-1 w-auto object-contain"
+          />
         </Link>
 
         <SignedIn>
@@ -22,13 +29,16 @@ const header = () => {
         </SignedIn>
       </nav>
 
-     <div className="flex gap-4 items-center">
-     <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-        <UserButton />
-      </SignedOut>
-     </div>
+      <div className="flex gap-4 items-center">
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton />
+          <UserButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </header>
   );
 };
