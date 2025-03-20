f7d3bc0a479375069b8134dbd400c887c59ec9ba
Nitesh-DK <nittesh.dk@gmail.com>
2025-03-20 16:29:09 +0530

    done

--- src/app/api/inngest/route.js
+++ src/app/api/inngest/route.js
@@ -1,13 +1,12 @@
 import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/functions";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-      /* your functions will be passed here later! */
-      helloWorld,
+    /* your functions will be passed here later! */
+    generateIndustryInsights,
   ],
 });
-
--- src/lib/inngest/client.js
+++ src/lib/inngest/client.js
@@ -1,5 +1,12 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "aspire-1212",name:"aspire" });
-
+export const inngest = new Inngest({
+  id: "aspire-1212",
+  name: "aspire",
+  credentials: {
+    gemini: {
+      apiKey: process.env.GEMINI_API_KEY,
+    },
+  },
+});
