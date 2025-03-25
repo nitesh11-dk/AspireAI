7413bcb216f8aaf24eb62b476820e73d59a48450
Nitesh-DK <nittesh.dk@gmail.com>
2025-03-25 15:54:23 +0530

    interivew quiz feature done

--- src/app/(main)/interview/page.jsx
+++ src/app/(main)/interview/page.jsx
@@ -1,14 +1,23 @@
-"use client";
-import React, { useEffect } from "react";
-import { generateQuiz } from "../../../../actions/interview";
+import { getAssessments } from "../../../../actions/interview";
+import StatsCards from "./_components/stats-cards";
+import PerformanceChart from "./_components/performace-chart";
+import QuizList from "./_components/quiz-list";
 
-const Interview = () => {
-  useEffect(() => {
-    let res = generateQuiz();
-    console.log(res);
-  });
+export default async function InterviewPrepPage() {
+  const assessments = await getAssessments();
 
-  return <div>Interview</div>;
-};
-
-export default Interview;
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-6xl font-bold gradient-title">
+          Interview Preparation
+        </h1>
+      </div>
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
+}
