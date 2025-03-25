eff3607454c6368ceaefaca3bc5b85bbf152a6c5
Nitesh-DK <nittesh.dk@gmail.com>
2025-03-25 16:16:06 +0530

    refactor: update image component and adjust logo size in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -30,13 +30,13 @@ const header = async () => {
     <header className="flex top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href={"/"}>
-          <img
+          <Image
             src={"/logo.gif"}
             width={200}
             height={200}
             alt="
         Aspire logo"
-            className="h-16 py-1 w-auto object-contain"
+            className="h-12 py-1 w-auto object-contain"
           />
         </Link>
 
